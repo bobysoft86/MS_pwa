b67a4abe093c80c8b7ae3dca18d9524d02d5fce8
Robert Rodriguez Carton <robertrodriguezcarton@gmail.com>
2025-10-11 09:28:44 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ms-pwa',
+  webDir: 'www'
+};
+
+export default config;
