9b62c27ef79ac52eec9e7ab0ec5ba370cc106b54
Robert Rodriguez Carton <robertrodriguezcarton@gmail.com>
2025-10-16 12:54:37 +0200

    arriba katana

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ export const environment = {
    * URL base del backend en producción
    * Ejemplo: tu dominio o instancia en la nube
    */
- apiUrl: 'http://103.45.245.67:3000',
-  assetsUrl: 'http://103.45.245.67:3000/uploads',
+ apiUrl: 'http://45.147.251.120/api',
+  assetsUrl: 'http://45.147.251.120/uploads',
   appName: 'MS Backoffice',
   version: '1.0.0',
 };
