40a4722da276676541e75e6cdedb94d4ee834827
Robert Rodriguez Carton <robertrodriguezcarton@gmail.com>
2025-10-16 18:54:50 +0200

    applied https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@ export const environment = {
    * URL base de tu API local
    * Ajusta el puerto si tu backend escucha en otro (por defecto: 3000)
    */
-  apiUrl: 'http://45.147.251.120/api',
-  assetsUrl: 'http://45.147.251.120/uploads',
+  apiUrl: 'https://45-147-251-120.sslip.io/api',
+  assetsUrl: 'https://45-147-251-120.sslip.io/uploads',
 
 
   appName: 'MS Backoffice',
